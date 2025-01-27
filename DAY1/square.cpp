5dc06d50fb2734f85f135ebdf0ad94f74b68e57c
Dilan <dilandhakal889@gmail.com>
2025-01-27 14:39:05 +0545

    code for day one

---
+++ DAY1/square.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Square from number:"<<endl;
+    for(int i=1;i<=4;i++){
+        for(int  j=1;j<=4;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"square from star:"<<endl;
+        for(int i=1;i<=4;i++){
+        for(int  j=1;j<=4;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DSA
+# HI
+I make this repo so that i can use it later for revision or to look back that how far i have come 
+and Hope   i won't quit it soon # Best of luck  to future me Hope i can make you  future 
+and guess what i got girlfriend and i wanna be good for her to be a better person so  im staring to learn so that
+i can atlease be a better person for her 
